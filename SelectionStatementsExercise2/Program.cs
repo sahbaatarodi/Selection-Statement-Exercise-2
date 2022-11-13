2b0019db44a4e7ef404a8134fa0ede97154ecb7d
sahba atarodi <sahba.atarodi@gmail.com>
2022-11-13 10:45:48 -0500

    initial commit

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite subject?");
+
+            string subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                case "math":
+                case "calculus":
+                    Console.WriteLine("I hate Math!");
+                    break;
+                case "science":
+                case "physics":
+                case "biology":
+                    Console.WriteLine("I love Sciene!");
+                    break;
+                case "pe":
+                case "physical education":
+                    Console.WriteLine("PE is the Best!");
+                    break;
+                case "history":
+                    Console.WriteLine("History is Fun1");
+                    break;
+                case "english":
+                    Console.WriteLine("English is Boring!");
+                    break;
+                default:
+                    Console.WriteLine("I don't know that subject.");
+                    break;
+
+            }
         }
     }
 }
